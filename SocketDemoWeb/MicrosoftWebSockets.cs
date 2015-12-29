cbd924c4b9e37df2f01d7c4fa6e90e915ea51f43
Eddy Lin <lin-shirui@hotmail.com>
2015-12-30 02:54:01 +0800

    新建项目

---
+++ SocketDemoWeb/MicrosoftWebSockets.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.WebSockets;
+
+namespace SocketDemoWeb
+{
+    public class MicrosoftWebSockets:WebSocketHandler
+    {
+        private static WebSocketCollection clients = new WebSocketCollection();
+
+        private string name;
+
+        public override void OnOpen()
+        {
+            name = this.WebSocketContext.QueryString["name"];
+            clients.Add(this);
+            clients.Broadcast(name + " is connented");
+        }
+
+        public override void OnClose()
+        {
+            base.OnClose();
+        }
+
+        public override void OnMessage(string message)
+        {
+            clients.Broadcast(message);
+        }
+
+
+        public override void OnError()
+        {
+            clients.Remove(this);
+        }
+
+    }
+}
---
+++ SocketDemoWeb/WebSocketServer.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.WebSockets;
+
+namespace SocketDemoWeb
+{
+    public class WebSocketServer : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if(context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(new MicrosoftWebSockets());
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
